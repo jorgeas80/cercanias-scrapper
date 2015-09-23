20e5e1fbc39476c2f570a1e6a7ac9e9db297142c
Jorge Arévalo <jorgeas80@gmail.com>
2015-09-24 00:56:37 +0200

    Parsing nucleos. Work in progress

--- cercanias/cercanias/spiders/nucleos.py
+++ cercanias/cercanias/spiders/nucleos.py
@@ -13,21 +13,34 @@ class NucleosSpider(scrapy.Spider):
     def parse(self, response):
 
         # Get nucleos by columns
-        for nucleo_link in response.xpath('//div[@id="colB"]/div[@class="colB1"]'):
-            pass
-
-
-
-        """
-        for nucleo_link in response.xpath('//div[@id="menu_lat"]/ul/li/a/@href').extract():
-
-            if nucleo_link == 'http://cercanias.blog.renfe.com/':
+        for nucleo_div in response.xpath('//div[@id="colB"]/div[@class="colB1" or @class="colB2" or @class="colB3"]'):
+            
+            # Get nucleo name
+            nucleo_name_array = nucleo_div.xpath('/h3/a/@title').extract()
+            if not nucleo_name_array or not isinstance(nucleo_name_array, list) or len(nucleo_name_array) <= 0:
                 continue
 
+
+            # Get link to nucleo page
+            nucleo_link_array = nucleo_div.xpath('h3/a/@href').extract()
+            if not nucleo_link_array or not isinstance(nucleo_link_array, list) or len(nucleo_link_array) <= 0:
+                continue
+
+            nucleo_link = nucleo_link_array[0]
+
+            # Follow the link to get nucleo id
             nucleo_url = response.urljoin(nucleo_link)
 
-            yield scrapy.Request(nucleo_url, callback=self.parse_nucleo_url)
+            # Get link to image
+            nucleo_img_array = nucleo_div.xpath('p/a/img/@src').extract()
 
+            # TODO: To get the nucleo id, we need to follow nucleo_url
+
+            #yield scrapy.Request(nucleo_url, callback=self.parse_nucleo_url)
+
+    
+    
     def parse_nucleo_url(self, response)
         pass
-    """
+
+
